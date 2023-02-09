f86d7eebc447425c64d8b7831fea8dae6553188b
LuisL <dev.eng.luisluna@gmail.com>
2023-02-09 04:19:04 +0000

    version comenzando a ubicar y dar estilos al contenedor, HTML y JS listo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let container = document.querySelector(".container");
+
+let btn = document.getElementById("spin");
+
+let number = Math.ceil(Math.random() * 10000);
+
+btn.onclick = function() {
+  container.getElementsByClassName.transform = "rotate(" + number + "deg)";
+  number += Math.ceil(Math.random() * 10000);
 };
