e4ce3b6264b0c91348f921d69f6b86a75312d1bc
LuisL <dev.eng.luisluna@gmail.com>
2023-02-09 05:21:46 +0000

    version casi final

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ import "./style.css";
 
 let container = document.querySelector(".container");
 let btn = document.getElementById("spin");
-let number = Math.ceil(Math.random() * 10000);
+let number = Math.ceil(Math.random() * 100000);
 
 btn.onclick = function() {
   container.style.transform = "rotate(" + number + "deg)";
-  number += Math.ceil(Math.random() * 10000);
+  number += Math.ceil(Math.random() * 100000);
 };
