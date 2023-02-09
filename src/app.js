35ccd8fc091f7f2d80afe008d4710a6b6c49b46b
LuisL <dev.eng.luisluna@gmail.com>
2023-02-09 05:15:40 +0000

    version ffinalizada de la rueda, solo falta poner los titulos dentro de cada renglon y ajustarlos

--- src/app.js
+++ src/app.js
@@ -2,16 +2,11 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 let container = document.querySelector(".container");
-
 let btn = document.getElementById("spin");
-
 let number = Math.ceil(Math.random() * 10000);
 
 btn.onclick = function() {
-  container.getElementsByClassName.transform = "rotate(" + number + "deg)";
+  container.style.transform = "rotate(" + number + "deg)";
   number += Math.ceil(Math.random() * 10000);
 };
